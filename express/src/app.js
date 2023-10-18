625adc2e1b45ff61b395e2881cf492107d826c9d
FacuM87 <facundo.mingorance@hotmail.com>
2023-10-18 20:49:29 -0300

    pequeña mejora

--- express/src/app.js
+++ express/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import ProductManager from "./ProductManager.js";
 
 const app = express() 
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 const juan = new ProductManager("./db.json")
