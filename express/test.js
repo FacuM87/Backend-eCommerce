a1a7b1065a364446338fa27033d528aeefc7fd5a
FacuM87 <facundo.mingorance@hotmail.com>
2023-10-29 12:21:38 -0300

    incorpora utils.js

--- express/test.js
+++
@@ -1,6 +0,0 @@
-const carts = [{id:1, products:[{product:1, quantity:1}, {product:2, quantity:3}]},{id:2, products:[{product:1, quantity:4}, {product:2, quantity:5}]}]
-
-const cartById = carts.find(p => p.id === 2)
-
-
-console.log(cartById.products); 
