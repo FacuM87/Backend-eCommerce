ad4d90fc17f9dfe97e65d75ffc4312a5020db86f
FacuM87 <facundo.mingorance@hotmail.com>
2024-01-21 18:51:22 -0300

    pequeña correccion

--- express/src/middlewares/middlewares.js
+++ express/src/middlewares/middlewares.js
@@ -10,17 +10,18 @@ export const auth = (req, res, next) => {
     res.redirect('/')
 }
 
-export const checkAdminPermissions = (req, res, next) => { 
+export const checkAdminPermissions = (req, res, next) => {
+    console.log("validating admin role") 
     const sessionActive = req.session.user
     if (sessionActive == undefined) return res.send("Login please")
-    if(req.session?.user.role !== "admin") return res.send("Not allowed")
+    if(req.session.user.role !== "admin") return res.send("Not allowed")
     next()
 }
 
 export const checkUserPermissions = (req, res, next) => {
-    console.log("checking user role");
+    console.log("validating user role")
     const sessionActive = req.session.user
     if (sessionActive == undefined) return res.send("Login please")
-    if(req.session?.user?.role !== "user") return res.send("Not allowed")
+    if(req.session.user.role !== "user") return res.send("Not allowed")
     next()
 }
