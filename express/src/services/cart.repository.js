563ec78d00e713ddc786cee21d26ae5365575efc
FacuM87 <facundo.mingorance@hotmail.com>
2024-01-22 21:56:56 -0300

    ÚLTIMO COMMIT ANTES DE IMPLEMENTAR CART REPOSITORY

--- express/src/services/cart.repository.js
+++ express/src/services/cart.repository.js
@@ -3,4 +3,18 @@ export default class CartRepository {
     constructor(dao){
         this.dao=dao
     }
+
+    createNewCart = async () =>{ return await this.dao.create() } 
+
+    getCarts = async () =>{ return await this.dao.get()} 
+
+    getCartByID = async (cartId) =>{ return await this.dao.getById(cartId)}
+
+    getPopulatedCart = async (cartId) =>{
+        return await this.getCartByID(cartId).populate("products.product").lean().exec();
+    }
+
+    updateCart = async (cartId, changes) =>{ return await this.dao.update(cartId, changes) }
+
+    deleteCart = async (cartId) => { return await this.dao.delete(cartId) }
 }
