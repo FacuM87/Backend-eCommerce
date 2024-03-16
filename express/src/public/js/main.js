491fc4d60ee5975a08e80ba78d4a2bed1c5ccc5d
FacuM87 <facundo.mingorance@hotmail.com>
2024-03-16 11:55:12 -0300

    mejora en main

--- express/src/public/js/main.js
+++ express/src/public/js/main.js
@@ -1,10 +1,20 @@
 
 
 document.getElementById("cartIcon").onclick = () =>{
-    const mainCartId = document.querySelector(".mainCartId").value
+    const mainCartId = document.getElementById("mainCartId").value
     console.log(mainCartId);
 
     if (!mainCartId) { return console.log("No cartId is available.") }
 
     document.location.href = `/cart/${mainCartId}`
+}
+
+const userRoleElement = document.getElementById("userRole")
+if (userRoleElement){
+    const userRoleValue = userRoleElement.value
+    const realTimeBtn = document.getElementById("RTProductsBTN")
+
+    if (userRoleValue == "admin" || userRoleValue =="premium") {
+        realTimeBtn.classList.remove("d-none")
+    }
 }
